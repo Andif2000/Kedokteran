f1f83c01007e91e94d252f64a8306d6227ce2960
Andi Fahmi <andifahmi266@gmail.com>
2022-12-25 09:38:09 +0800

    Menambahkan Syntax Pada App

--- App.js
+++ App.js
@@ -1,12 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import Route from './Src/Route';
+import Ionicons from '@expo/vector-icons/Ionicons'
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Route />
   );
 }
 
