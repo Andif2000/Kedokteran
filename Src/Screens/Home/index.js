c9d7427b05ee68b1f8fe437a58f8fc96de2ae264
Andi Fahmi <andifahmi266@gmail.com>
2022-12-26 08:29:41 +0800

    Menambahkan Syntax Pada Screen Home

--- Src/Screens/Home/index.js
+++ Src/Screens/Home/index.js
@@ -1,14 +1,43 @@
-import { View, Text, StyleSheet } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet, Touchable, TouchableOpacity, TextInput } from 'react-native'
+import React, { useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import Ionicons from '@expo/vector-icons/Ionicons'
 import { StatusBar } from 'expo-status-bar'
 
+import SearchCustom from '../../Components/SearchCustom'
+
 const Home = () => {
+    const [search, setSearch] = useState('');
     return (
         <SafeAreaView style={styles.container}>
-            <View>
-                <Text>Hello</Text>
+            <View
+                style={{
+                    marginHorizontal: 20,
+                    marginTop: 10,
+                    flexDirection: 'row',
+                    alignItems: 'center',
+                    justifyContent: 'space-between'
+                }}>
+                <View>
+                    <Text
+                        style={{
+                            color: '#000080',
+                            fontWeight: '600'
+                        }}>Hello</Text>
+                    <Text
+                        style={{
+                            color: '#000080',
+                            fontSize: 22, fontWeight: 'bold'
+                        }}>Andif20 👋</Text>
+                </View>
+                <TouchableOpacity>
+                    <Ionicons
+                        name='person-circle-outline' 
+                        size={40} 
+                        style={{color:'#000080'}}/>
+                </TouchableOpacity>
             </View>
+            <SearchCustom placeholder='Cari Informasi Tentang Kesehatan/Dokter' />
         </SafeAreaView>
     )
 }
