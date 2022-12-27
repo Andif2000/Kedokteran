7f4977c242740c09a442a37525410bd56dab6df5
Andi Fahmi <andifahmi266@gmail.com>
2022-12-27 21:15:41 +0800

    Membuat Database

---
+++ Src/Components/Database.js
@@ -0,0 +1,47 @@
+export const doctor_schedule = [
+    {
+        id: 1,
+        name: 'dr. Ahmad Syafi`i',
+        skill : 'Speasialis Bedah',
+        date: '11 Januari 2023',
+        time: '12.00',
+        clinic: 'Syafi`i Clinic',
+        picture: require('../../assets/Images/dokter1.jpg')
+    },
+    {
+        id: 2,
+        name: 'dr. Mujahidah',
+        skill : 'Spesialis Saraf',
+        date: '21 Januari 2023',
+        time: '15.00',
+        clinic: 'Assyifa Clinic',
+        picture: require('../../assets/Images/dokter2.jpg')
+    },
+    {
+        id: 3,
+        name: 'dr. Fikri',
+        skill : 'Spesialis Tulang',
+        date: '22 Januari 2023',
+        time: '10.00',
+        clinic: 'Clinic Medika Fikri',
+        picture: require('../../assets/Images/dokter3.jpg')
+    },
+    {
+        id: 4,
+        name: 'dr. Ananda Anisa',
+        skill : 'Spesialis Gigi',
+        date: '05 Januari 2023',
+        time: '16.00',
+        clinic: 'Cahaya Medika',
+        picture: require('../../assets/Images/dokter4.jpg')
+    },
+    {
+        id: 5,
+        name: 'dr. ZulFahmi',
+        skill : 'Spesialis Jantung',
+        date: '14 Januari 2023',
+        time: '19.00',
+        clinic: 'Smile Medika',
+        picture: require('../../assets/Images/dokter5.jpg')
+    },
+]
