6f53c88a0a74c0c6c9801857446c6149547c8a5c
Andi Fahmi <andifahmi266@gmail.com>
2022-12-28 08:05:19 +0800

    Membuat Data Pada Database

--- Src/Components/Database.js
+++ Src/Components/Database.js
@@ -2,7 +2,7 @@ export const doctor_schedule = [
     {
         id: 1,
         name: 'dr. Ahmad Syafi`i',
-        skill : 'Speasialis Bedah',
+        skill: 'Speasialis Bedah',
         date: '11 Januari 2023',
         time: '12.00',
         clinic: 'Syafi`i Clinic',
@@ -11,7 +11,7 @@ export const doctor_schedule = [
     {
         id: 2,
         name: 'dr. Mujahidah',
-        skill : 'Spesialis Saraf',
+        skill: 'Spesialis Saraf',
         date: '21 Januari 2023',
         time: '15.00',
         clinic: 'Assyifa Clinic',
@@ -20,7 +20,7 @@ export const doctor_schedule = [
     {
         id: 3,
         name: 'dr. Fikri',
-        skill : 'Spesialis Tulang',
+        skill: 'Spesialis Tulang',
         date: '22 Januari 2023',
         time: '10.00',
         clinic: 'Clinic Medika Fikri',
@@ -29,7 +29,7 @@ export const doctor_schedule = [
     {
         id: 4,
         name: 'dr. Ananda Anisa',
-        skill : 'Spesialis Gigi',
+        skill: 'Spesialis Gigi',
         date: '05 Januari 2023',
         time: '16.00',
         clinic: 'Cahaya Medika',
@@ -38,10 +38,64 @@ export const doctor_schedule = [
     {
         id: 5,
         name: 'dr. ZulFahmi',
-        skill : 'Spesialis Jantung',
+        skill: 'Spesialis Jantung',
         date: '14 Januari 2023',
         time: '19.00',
         clinic: 'Smile Medika',
         picture: require('../../assets/Images/dokter5.jpg')
+    },
+]
+
+export const Categori_List = [
+    {
+        id: 1,
+        name: 'Artikel'
+    },
+    {
+        id: 2,
+        name: 'Obat'
+    },
+    {
+        id: 3,
+        name: 'Kesehatan'
+    },
+    {
+        id: 4,
+        name: 'Dokter'
+    },
+    {
+        id: 5,
+        name: 'Klinik'
+    },
+    {
+        id: 6,
+        name: 'Rumah Sakit'
+    },
+]
+
+export const Articels = [
+    {
+        id: 1,
+        title: 'Cara Menjaga Pola Hidup Sehat',
+        description: 'inimo dulu karena dak tauka mau berkata-kata apa saya kodong, bukan ka puitisi',
+        picture: require('../../assets/Images/dokter1.jpg')
+    },
+    {
+        id: 2,
+        title: 'Mengonsumsi Buah ini Dapat Memperbaiki Sistem Saraf',
+        description: 'inimo dulu karena dak tauka mau berkata-kata apa saya kodong, bukan ka puitisi',
+        picture: require('../../assets/Images/dokter2.jpg')
+    },
+    {
+        id: 3,
+        title: 'Tips Agar Tulang Tetap Kokoh',
+        description: 'inimo dulu karena dak tauka mau berkata-kata apa saya kodong, bukan ka puitisi',
+        picture: require('../../assets/Images/dokter3.jpg')
+    },
+    {
+        id: 4,
+        title: 'Lima Lankah Mudah Merawat Gigi Anda',
+        description: 'inimo dulu karena dak tauka mau berkata-kata apa saya kodong, bukan ka puitisi',
+        picture: require('../../assets/Images/dokter4.jpg')
     },
 ]
